39d1e5d6d04bbabed838f656cc42d5ca977943b0
Oscar Sixtos <sixtos04@gmail.com>
2022-12-28 01:49:08 -0600

    Create badge

---
+++ Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,38 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Oscar Sixtos on 28/12/22.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index)/8.0 * 360.0)
+            )
+        }
+        .opacity(0.5)
+    }
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0/4.0, anchor: .top)
+                    .position(
+                        x: geometry.size.width / 2.0,
+                        y: geometry.size.height * (3.0/4.0)
+                )
+            }
+        }
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
