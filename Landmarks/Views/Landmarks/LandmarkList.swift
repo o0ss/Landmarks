ac9b9c0e3c2bbe79ef3cb623b8ab744a562565c0
Oscar Sixtos <sixtos04@gmail.com>
2022-12-27 23:54:54 -0600

    Add Favorite button and state

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Oscar Sixtos on 25/12/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+//        ForEach(["iPhone 8", "iPhone 11"], id: \.self) { deviceName in
+//                LandmarkList()
+//                        .previewDevice(PreviewDevice(rawValue: deviceName))
+//                        .preferredColorScheme(.light)
+//                        .previewDisplayName(deviceName)
+//        }
+        Group {
+            LandmarkList()
+                .environmentObject(ModelData())
+        }
+        
+    }
+}
