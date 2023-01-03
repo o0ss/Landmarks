f061cb705b722cc974c2238459fea17dd4a251e6
Oscar Sixtos <sixtos04@gmail.com>
2023-01-03 17:18:55 -0600

    Add Apple Watch target. Add LandmarkDetail for AppleWatch

---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Oscar Sixtos on 03/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .symbolRenderingMode(.hierarchical)
+//                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
