15efc1ea2b10fb49daf4ec1c0b1d8e004c4c31ca
Oscar Sixtos <sixtos04@gmail.com>
2023-01-05 13:10:56 -0600

    Add Watch app notification

--- WatchLandmarks Watch App/ContentView.swift
+++ WatchLandmarks Watch App/ContentView.swift
@@ -9,19 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .symbolRenderingMode(.hierarchical)
-//                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
